7650d0c953bf84eae7cbe7d18b576eedd03b64ac
Alban Andrieu <alban.andrieu@mysis.com>
2014-10-25 02:35:29 +0200

    first commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Ansible managed: /workspace/users/albandri10/env/ansible/roles/jenkins-slave/templates/build.sh.j2 modified on 2014-09-25 11:57:18 by albandri on albandri-laptop-misys
+
+DOCKERNAME="nabla/ansible-shell"
+
+time docker build -t $DOCKERNAME .
+
+echo
+echo "This image is a trusted docker.io Image."
+echo
+echo "To pull it"
+echo "    docker pull $DOCKERNAME"
+echo
+echo "To use this docker:"
+echo "    docker run -d -P $DOCKERNAME"
+echo
+echo "To run in interactive mode for debug:"
+echo "    docker run -t -i $DOCKERNAME bash"
+echo
